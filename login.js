56ba7f8c13b0ed77c7e5294447f3364a65eb182b
doloshabell <habelldol@gmail.com>
2022-02-08 23:53:08 +0700

    add file index login ragister login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+const login = () => {
+    const usernameInput = document.querySelector("#email").value;
+    const passwordInput = document.querySelector("#password").value;
+
+    const dataLocalStorage = JSON.parse(localStorage.getItem);
+}
